fd49108f00d63b9482a42e51cf063ed14ecca8fd
Tom Savage <41382678+TomRSavage@users.noreply.github.com>
2019-04-04 13:31:42 +0100

    Separated utilities

--- EvolutionaryAlgorithm.py
+++ EvolutionaryAlgorithm.py
@@ -2,180 +2,12 @@
 import numpy as np 
 import numpy.random as rnd
 import copy 
-import matplotlib.pyplot as plt 
+from EvolutionaryAlgorithmUtils import *
 
-
-def Rastrigin(X):
-    '''INPUTS
-    X: arguments of the Rastrigin Function
-    OUTPUTS
-    f : evaluation of the Rastrigin function given the inputs
-    
-    DOMAIN         : [-5.12,5.12]
-    DIMENSIONS     : any
-    GLOBAL MINIMUM : f(x)=0 x=[0,...,0] 
-'''
-    an=10*len(X)
-    f_sum=sum(X[i]**2-(10*np.cos(2*np.pi*X[i]))for i in range(len(X)))
-    return an+f_sum
-
-def initiation(f,bounds,p):
-    '''
-    Creates a set of random particles, and evaluates their fitness
-    within the bounds of the function f
-
-    INPUTS
-    f       : function to be searched over
-    bounds  : bounds of function in form [[x1,x2],[x3,x4],[x5,x6]...]
-    p       : number of particles
-    
-    OUTPUTS
-    particle_pos      : array of random particle positions 
-    particle_pos_val  : fitness of each particle 
-    population_best   : position of best particle
-    
-    '''
-    d=len(bounds) #finding number of dimensions
-    particle_pos=np.zeros(p) #creating empty position array
-    particle_pos=particle_pos.tolist() #converting array to list
-    particle_pos_val=particle_pos[:] #empty value array
-    for j in range(p): #iterating ovre the number of particles
-        particle_pos[j]=[rnd.uniform(bounds[i][0],bounds[i][1])\
-                    for i in range(d)] #random coordinate within bounds
-        particle_pos_val[j]=f(particle_pos[j]) #calculating function value
-                                            #at each particle
-    population_best=particle_pos[np.argmin(particle_pos_val)]
-    return d,np.array(particle_pos), np.array(particle_pos_val)\
-        ,np.array(population_best)
-
-def fitnesses(f,p,particle_pos):
-    '''
-    Returns fitnesses of all particles 
-
-    INPUTS: 
-    f               : function to be optimized
-    p               : population size 
-    particle_pos    : matrix of all particle's positions
-
-    OUTPUTS: 
-    particle_pos_val: array of all particle's fitnesses
-
-    '''
-    particle_pos_val=np.zeros(p)
-    for i in range(p):
-        particle_pos_val[i]=f(particle_pos[i])
-    return particle_pos_val
-
-
-
-def particlesort(p,particle_pos,particle_pos_val,d): 
-    '''
-    Sorts all particles based on their fitness 
-
-    INPUTS 
-    p               : number of particles
-    particle_pos    : matrix of particle positions 
-    particls_pos_val: array of particle fitnesses
-    d               : dimensions
-
-    OUTPUTS 
-    particle_pos_sorted     : matrix of particles sorted from best to worst fitness
-    particle_pos_val_sorted : matrix of particle fitnesses
-                              sorted from best to worst fitness
-    '''
-    sorted_index=np.argsort(particle_pos_val) #sorts indexes
-    particle_pos_sorted=np.zeros((p,d)) #creating empty new array
-    particle_pos=np.array(particle_pos)
-    particle_pos_sorted=particle_pos[:]
-    particle_pos_val_sorted=particle_pos_val[:]
-    for i in range(p): #re-arranges positions based on sorted fitnesses 
-        particle_pos_sorted[i,:]=particle_pos[sorted_index[i],:]
-        particle_pos_val_sorted[i]=particle_pos_val[sorted_index[i]]
-    return particle_pos_sorted,particle_pos_val_sorted
-
-def selection(particle_pos,p):
-    ''' 
-    Culls 50% of particles 
-
-    INPUTS: 
-    particle_pos : matrix of all particle positions
-    p            : number of particles 
-
-    OUTPUTS:
-    particle_pos : new matrix of particle positions after 
-                   culling 50% 
-    p            : new number of particles 
-
-    '''
-    particle_pos=particle_pos[:int(p/2)] #truncating lower half of particles
-    p=len(particle_pos) #calculating new number
-    return particle_pos,p
-
-
-def crossover(male_particle,female_particle):
-    '''
-    Takes two particles (male and female), and performs a 
-    crossover operation with one split. 
-    The split is randomly chosen at a position along the dimensions
-    the male and female coordinates before this split are then swapped 
-    https://en.wikipedia.org/wiki/Crossover_(genetic_algorithm)
-
-    INPUTS: 
-    male particle       : d dimensional array of coordinates
-    female_particle     : d dimensional array of coordinates
-
-    OUTPUTS: 
-    male_particle       : crossed over set of male coordinates
-    female_particle     : crossed over set of male coordinates
-    '''
-    male_copy=male_particle[:]
-    female_copy=female_particle[:]
-    d=len(female_particle)
-
-    random_split=rnd.randint(0,d)
-
-    for i in range(random_split):
-        male_particle[i]=female_copy[i]
-    for i in range(random_split,d):
-        female_particle[i]=male_copy[i]
-
-    return male_particle,female_particle
-
-def mutation(d,particle_pos,bounds):
-    '''
-    Takes a particle, and with a 1% probability per dimension,
-    gives that dimension a random coordinate within bounds
-
-    INPUTS: 
-    d               : number of dimensions
-    particle_pos    : d dimensional array of coordinates 
-    bounds          : bounds of the problem ([[xl,xu],[xl,xu]...])
-
-    OUTPUTS: 
-    particle_pos    : d dimensional array of mutated coordinates
-
-    '''
-    prob=rnd.rand(d) #creates random numbers between 0-1
-    for i in range(d): #for each dimension if the random number is greater than 0.99
-        if prob[i]>0.975: #give random coordinate in that dimension 
-            particle_pos[i]=rnd.uniform(bounds[i][0],bounds[i][1])
-    return particle_pos
-
-def plotutility(fstore,iterations):
-    it_array=np.linspace(0,iterations,iterations)
-    plt.figure()
-    plt.xlabel('generations')
-    plt.ylabel('best population fitness')
-    plt.plot(it_array,fstore)
-    plt.show()
-    return
-
-
-
-def evolution(f,bounds,p,iterations):
+def evolution(f,bounds,p,iterations,cull_percen,mut_percen,plot=False):
     '''
     Performs initial evolution step: 
-    initializes particles, 
+    initializes individuals, 
     selects best candidates
     performs cross-over
     creates children
@@ -190,62 +22,85 @@ def evolution(f,bounds,p,iterations):
     iterations: number of generations 
 
     OUTPUTS: 
-    returns the best particle after specified iterations 
+    returns the best individual after specified iterations 
     as well as a plot of function value per generation 
     '''
+    p_init=p
+    d,individual_pos,individual_pos_val,population_best=\
+        initiation(f,bounds,p) #creates population
 
-    d,particle_pos,particle_pos_val,population_best=\
-        initiation(f,bounds,p)
-    
-    fstore=[]
+    if plot==True: #creating function store array if plot wanted
+        fstore=[]
 
-    for it in range(iterations):
-        particle_pos,particle_pos_val=\
-            particlesort(p,particle_pos,particle_pos_val,d)
-        particle_pos,p=selection(particle_pos,p)
+    for it in range(iterations): 
+        individual_pos,individual_pos_val=\
+            individualsort(p,individual_pos,individual_pos_val,d) #sort all individuals
+            #based on function value (low to high)
 
+        individual_pos,p=selection(individual_pos,p,cull_percen) #select the best individuals
+        while p<p_init:
+            individual_pos=np.concatenate((individual_pos,individual_pos),axis=0)
+            p=len(individual_pos)
+        individual_pos=individual_pos[:p_init]
+        p=len(individual_pos)
+        #as the population is now half the size, it is duplicated 
+        #I guess this would be similar to creating twins 
+
+        '''
+        This next section of code performs the cross over. 
+        It first 'shuffles' the second half of individuals, then iterates over 
+        the population performing the crossover function on the first individual in the
+        population, with the first individual in the shuffled second half of the 
+        population. It then replaces the original individuals with the crossed over 
+        coordinates.
+        '''
         for i in range(int(p/2)):
-            female_index=list(range(0,int((p/2))))
-            rnd.shuffle(female_index)
-            male_particle=particle_pos[i,:]
-            female_particle=particle_pos[int((p/2))+female_index[i],:]
-            male_particle,female_particle=\
-                crossover(male_particle,female_particle)
-            particle_pos[i,:]=male_particle[:]
-            particle_pos[int(p/2)+female_index[i],:]=female_particle[:]
+            female_index=list(range(0,int((p/2)))) 
+            rnd.shuffle(female_index) #shuffling second half
+            male_individual=individual_pos[i,:] #selecting first individual
+            female_individual=individual_pos[int((p/2))+female_index[i],:]
+            #selecting first individual in second half
+            male_individual,female_individual=\
+                crossover(male_individual,female_individual) #performing crossover
+            individual_pos[i,:]=male_individual[:] #replacing individuals
+            individual_pos[int(p/2)+female_index[i],:]=female_individual[:]
 
-        particle_pos=np.concatenate((particle_pos,particle_pos),axis=0)
-        p=len(particle_pos)
-        
         for i in range(p):
-            particle_pos[i]=mutation(d,particle_pos[i],bounds)
+            individual_pos[i]=mutation(d,individual_pos[i],bounds,mut_percen)
 
-        particle_pos_val=fitnesses(f,p,particle_pos)
-        population_best=particle_pos[np.argmin(particle_pos_val)]
+        #evaluating new fitnesses of the population and getting the function 
+        #value of the best individual
+        individual_pos_val=fitnesses(f,p,individual_pos)
+        population_best=individual_pos[np.argmin(individual_pos_val)]
         best_val=f(population_best)
-        print('current best individual: ',best_val)
-        fstore.append(best_val)
+        #displaying best function value
+        print('Iteration number: ',it,' Current best fitness: ',best_val)
+
+        if plot==True: #adding best value to function store array 
+            fstore.append(best_val)
     
+    if plot==True: 
+        plotutility(fstore,iterations)
+
     print('after ',iterations,' iterations, optimum at: ',population_best)
-    plotutility(fstore,iterations)
+
     return 
 
-dimensions=5
-dimension_bounds=[-5,5]
-bounds=[0]*dimensions #creating 5 dimensional bounds
+dimensions=15  #setting dimensions of problem (# of variables)
+dimension_bounds=[-3,3]  #setting bounds (in this case a hypercube)
+bounds=[0]*dimensions 
 for i in range(dimensions):
-    bounds[i]=dimension_bounds
-f=Rastrigin 
-iterations=1000
-p=50
+    bounds[i]=dimension_bounds #creating bounds in the form [[xl,xu],[xl,xu]...]
+f=rosenbrock#function to be optimized 
+iterations=2000 #number of iterations
+cull_percen=0.75
+mut_percen=0.95
+p=60 #population size
 
-'''
-This is just a test situation, Sphere function performed over 
-30 dimensions, with a population size of 50, and a generation count 
-of 500
-'''
 
-evolution(f,bounds,p,iterations)
+evolution(f,bounds,p,iterations,cull_percen,mut_percen,plot=True) 
+
+
 
     
 
--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # Evolutionary Algorithm within Python
 
-<<<<<<< HEAD
 <img src="https://github.com/TomRSavage/EvolutionaryAlgorihtm/blob/master/Sty.gif" width="400"> <img src="https://github.com/TomRSavage/ParticleSwarm/blob/master/StyFUNC.gif" width="400"> 
-=======
 <img src="https://github.com/TomRSavage/EvolutionaryAlgorithm/blob/master/RastriginEvolution.gif" width="400"> <img src="https://github.com/TomRSavage/EvolutionaryAlgorithm/blob/master/RastriginEvolutionFUNC.gif" width="400"> 
->>>>>>> 2faa205dbd7e21806db5e4f957d4b5a1c4a48728
+
+
+## Evolutionary Algorithm Background
+
+
+### Current implimentation
+
+
+ ### Limitations
+
+
+
+### Prerequisites
+
+Python 3.0 is required. The ParticleSwarmUtility.py file must be in the same directory as the ParticleSwarm.py file in order to enable the utility to be used.
+
+## Function Use
+
+## Example
 
 
 ## Authors
 
 * **Tom Savage** - *Initial work* - [TomRSavage](https://github.com/TomRSavage)
+
+
